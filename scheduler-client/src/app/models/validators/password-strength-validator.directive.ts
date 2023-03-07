fbe446bf00e5441fc8611e33508f26251ff252e6
Anton Erne <antonerne@yahoo.com>
2023-03-07 16:10:07 +0900

    Mar07-1610

--- scheduler-client/src/app/models/validators/password-strength-validator.directive.ts
+++ scheduler-client/src/app/models/validators/password-strength-validator.directive.ts
@@ -11,7 +11,6 @@ import { AbstractControl, NG_VALIDATORS, Validator } from '@angular/forms';
 })
 export class PasswordStrengthValidator implements Validator {
   validate(control: AbstractControl) : {[key: string]: any} | null {
-    control.
     const value: string = (control.value && control.value !== null) 
       ? control.value : '';
     if (value === '' || value === null) {
